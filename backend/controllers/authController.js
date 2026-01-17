59a2db005df638a6d2c0513548a9ecc6cdbb6c48
Vidushi119 <vidushi.gupta.csds21@ggits.net>
2026-01-17 20:59:29 +0530

    Phase 6 Step 6.3: login API with JWT

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const asyncHandler = require("../middleware/asyncHandler");
+const generateToken = require("../utils/generateToken");
 
 /**
  * @desc    Register new user
@@ -8,26 +9,51 @@ const asyncHandler = require("../middleware/asyncHandler");
 const registerUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  // validation
   if (!email || !password) {
     res.status(400);
     throw new Error("Email and password are required");
   }
 
-  // check existing user
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
     throw new Error("User already exists");
   }
 
-  // create user
   const user = await User.create({ email, password });
 
   res.status(201).json({
     _id: user._id,
     email: user.email,
+    token: generateToken(user._id),
   });
 });
 
-module.exports = { registerUser };
+/**
+ * @desc    Login user
+ * @route   POST /auth/login
+ */
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Email and password are required");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.status(200).json({
+      _id: user._id,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+module.exports = { registerUser, loginUser };
+
